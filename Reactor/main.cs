1191a3737fb4c63be50d1e9b1bcf158cb7b3e7e2
gosling228 <greatking123456798qwerty@gmail.com>
2021-04-23 10:15:53 +0300

    Reactor/

---
+++ Reactor/main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Reactor
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Boolean enabled = true;
+            reactor myReactor = new reactor(enabled);
+            Thread ThUp = new Thread(new ThreadStart(myReactor.tempUp));
+            Thread ThDown = new Thread(new ThreadStart(myReactor.tempDown));
+            Thread ThReactorMonitor = new Thread(new ThreadStart(myReactor.reactorMonitor));
+            Thread ThStop = new Thread(new ThreadStart(myReactor.stopReactor));
+            ThUp.Start();
+            ThDown.Start();
+            ThReactorMonitor.Start();
+            ThStop.Start();
+        }
+    }
+}
---
+++ Reactor/reactor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace Reactor
+{
+    class reactor
+    {
+        private object ReactorLock = new object();
+        Int32 temp = 30;
+        Int32 targetTemp = 80;
+        Boolean enabled = true;
+
+        public reactor(Boolean enabled) { this.enabled = enabled; }
+
+        public void tempUp()
+        {
+            while (enabled)
+            {
+                lock (ReactorLock)
+                {
+                    temp++;
+                    Console.WriteLine(temp);
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public void tempDown()
+        {
+            while (enabled || temp > 0)
+            {
+                lock (ReactorLock)
+                {
+                    if (temp > targetTemp)
+                    {
+                        int diff = Math.Abs(targetTemp - temp);
+                        if (diff < 10) temp -= 2;
+                        else if (diff > 10 && diff < 20) temp -= 3;
+                        else if (diff > 20) temp -= 4;
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public void reactorMonitor()
+        {
+            while (enabled || temp > -1)
+            {
+                Console.WriteLine("temp:" + temp);
+                if (temp >= 100)
+                {
+                    Console.Clear();
+                    Console.WriteLine("explosion");
+                }
+                else if (temp <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("stop");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void stopReactor()
+        {
+            while (enabled)
+            {
+                var s = Console.ReadLine();
+                var line = s;
+                if (line.Equals("stop")) Environment.Exit(0);
+                else targetTemp = int.Parse(line);
+                Thread.Sleep(100);
+            } 
+        }
+    }
+}
